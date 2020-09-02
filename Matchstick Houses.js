686d75a3b967b4c6fca9d06863d14fecf1b61ac3
Mirzokhidd <50044566+Mirzokhidd@users.noreply.github.com>
2020-09-02 21:21:00 +0400

    Add files via upload

---
+++ Matchstick Houses.js
@@ -0,0 +1,21 @@
+/*Program which calculates how many sticks would be there in a stick house. One house is 6 sticks but as the amount sticks can 
+be calculated with (5 x **value**) - 1:
+
+
+examples:
+(1) = 6
+(2) = 11
+*/
+
+
+function matchHouses(step) {
+	if (step === 0) {
+		return 0;
+	} else if(Math.sign(step) === -1) {
+		return "";
+	}
+	var result = (5 * step) + 1;
+	return result;
+}
+
+console.log(matchHouses(2))
