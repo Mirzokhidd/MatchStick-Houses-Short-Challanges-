86472bc6a35217d63ba8fed995f71b6a484159c2
Mirzokhidd <50044566+Mirzokhidd@users.noreply.github.com>
2020-10-08 07:39:36 +0400

    Update Matchstick Houses.js

--- Matchstick Houses.js
+++ Matchstick Houses.js
@@ -1,3 +1,8 @@
+//You can run the code through the terminal by specifying the path and running the text, "node java.ja"
+//Or you can just run it rhought the 'Run code' to output function of visual studious
+
+//-------------------------------------------------------------------------------------------------------------
+
 /*Program which calculates how many sticks would be there in a stick house. One house is 6 sticks but as the amount sticks can 
 be calculated with (5 x **value**) - 1:
 
